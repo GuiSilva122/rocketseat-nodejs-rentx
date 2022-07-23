26ecc884280152d34622a0efdb94b42fe59455db
Guilherme de Souza Silva <gui.iug.net@gmail.com>
2022-07-23 12:30:46 -0300

    User authentication

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -9,14 +9,19 @@ class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
+  
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+    return user;
+  }
 
   async findByEmail(email: string): Promise<User> {
     const user = await this.repository.findOne({ email });
     return user;
   }
 
-  async create({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
-    const user = await this.repository.create({ name, email, password, driver_license });
+  async create({ name, email, password, driver_license, avatar, id }: ICreateUserDTO): Promise<void> {
+    const user = await this.repository.create({ name, email, password, driver_license, avatar, id });
     await this.repository.save(user);
   }
 }
