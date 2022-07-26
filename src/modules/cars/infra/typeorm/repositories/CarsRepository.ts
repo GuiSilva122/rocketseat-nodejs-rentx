198a6f69d35ea2097ea45397ab8ec14c59416c0b
Guilherme de Souza Silva <gui.iug.net@gmail.com>
2022-07-26 11:20:38 -0300

    Refactoring repository

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -14,9 +14,9 @@ class CarsRepository implements ICarsRepository {
       .createQueryBuilder("c")
       .where("available = :available", { available: true});
 
-    if (brand) carsQuery.andWhere("c.brand = :brand", { brand});
-    if (name) carsQuery.andWhere("c.name = :name", { name});
-    if (category_id) carsQuery.andWhere("c.category_id = :category_id", { category_id});
+    if (brand) carsQuery.andWhere("brand = :brand", { brand});
+    if (name) carsQuery.andWhere("name = :name", { name});
+    if (category_id) carsQuery.andWhere("category_id = :category_id", { category_id});
 
     const cars = await carsQuery.getMany();
     return cars;
