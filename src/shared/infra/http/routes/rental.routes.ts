97b68e4bedc2c00041493911def3eaabf2c15182
Guilherme de Souza Silva <gui.iug.net@gmail.com>
2022-07-28 17:18:39 -0300

    Adding list rentals by user use case

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,5 +1,6 @@
 import { CreateRentalsController } from '@modules/rentals/useCases/createRental/CreateRentalsController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
 import { Router } from 'express';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -7,8 +8,10 @@ const rentalRoutes = Router();
 
 const createRentalsController = new CreateRentalsController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/",  ensureAuthenticated, createRentalsController.handle);
+rentalRoutes.get("/user",  ensureAuthenticated, listRentalsByUserController.handle);
 rentalRoutes.post("/devolution/:id",  ensureAuthenticated, devolutionRentalController.handle);
 
 export { rentalRoutes };
